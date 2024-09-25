c7f4f9453dbc86354e4de92f0a3b29cfeb44249f
Zain Raza <zainraza876@gmail.com>
2024-09-25 16:46:56 +0000

    This is first Terraform Project

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
+}
